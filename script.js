22166c6cb2f76b2777e0328c95ba7c424d50f8b7
happymix <happymix2555@gmail.com>
2022-11-14 17:09:04 +0700

    rock paper scissors finished.

--- script.js
+++ script.js
@@ -17,6 +17,9 @@ function playRound(playerSelection, computerSelection){
         ['paper', 'scissors', 'rock'],
         ['scissors', 'rock', 'paper']
     ];
+    if (_computerSelection === _playerSelection){
+        return(['Draw!', 2]);
+    }
     for (let i = 0; i < 3; i++) {
         // console.log(currentWinnerAndLoser[i]);
         // console.log(i);
@@ -24,16 +27,50 @@ function playRound(playerSelection, computerSelection){
             for (let j = 1; j < 3; j++) {
                 if (currentWinnerAndLoser[i][j] === _computerSelection && j === 1){
                     // return('computer won');
-                    return('You Lose! ' + _computerSelection.toUpperCase() + ' beats ' + _playerSelection.toUpperCase());
+                    return(['You Lose! ' + _computerSelection.toUpperCase() + ' beats ' + _playerSelection.toUpperCase(), 0]);
                 }
                 else {
-                    return('You Win! ' + _playerSelection.toUpperCase() + ' beats ' + _computerSelection.toUpperCase());
+                    return(['You Win! ' + _playerSelection.toUpperCase() + ' beats ' + _computerSelection.toUpperCase(), 1]);
                 }
             }
         }
     }
 }
-let item = getComputerChoice();
+
+function game(){
+    let computerScore = 0;
+    let playerScore = 0;
+    //loop 5 times
+    for (let round = 0; round < 5; round++){
+        //take user input
+        let playerChoice = prompt("What's your choice?");
+        //take computer input
+        let computerChoice = getComputerChoice();
+        //find winner
+        let results = playRound(playerChoice, computerChoice);
+        //console.log the winner
+        console.log(results[0]);
+        //plus one count to the winner
+        if (results[1] === 0){
+            computerScore += 1;
+        } else if (results[1] === 1){
+            playerScore += 1;
+        } else {
+            //pass
+        }
+    }
+    //return the most win count
+    if (computerScore > playerScore) {
+        console.log('Cat will conquer the world!');
+    } else if (playerScore > computerScore){
+        console.log('Too soon kitty!');
+    } else {
+        console.log('Let cuddle meow meow.');
+    }
+
+}
+// let item = getComputerChoice();
 // console.log(item);
 // console.log(getComputerChoice());
-console.log(playRound('scissors', 'paper'));
+// console.log(playRound('scissors', 'paper'));
+game();
